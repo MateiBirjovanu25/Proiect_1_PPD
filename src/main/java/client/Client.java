7aabd67a0ddec511a725f9791a59192db64f9ac4
matei birjovanu <matei.ll64@gmail.com>
2023-01-11 15:14:17 +0200

    automat

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Objects;
+import java.util.Random;
 
 public class Client {
 
@@ -16,44 +17,30 @@ public class Client {
     public static void main(String[] args) throws IOException {
         BufferedReader stdIn =
                 new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("enter server host name");
-        String hostName = stdIn.readLine();
-
-        System.out.println("enter server port");
-        int portNumber = Integer.parseInt(stdIn.readLine());
+        Random rand = new Random();
+        String hostName = "localhost";
+        int portNumber = 1234;
 
         while (true) {
-            System.out.println("enter command");
-            String command = stdIn.readLine();
-            String result, commandString = command + "|";
+            String result, commandString;
 
-            if (Objects.equals(command, "exit")) break;
-            else if (Objects.equals(command, "programare")) {
-                System.out.println("nume: ");
-                String nume = stdIn.readLine();
-                System.out.println("cnp: ");
-                String cnp = stdIn.readLine();
-                System.out.println("data: ");
-                String data = stdIn.readLine();
-                System.out.println("locatie: ");
-                String locatie = stdIn.readLine();
-                System.out.println("tip tratament: ");
-                String tipTratament = stdIn.readLine();
-                System.out.println("ora start: ");
-                String oraStart = stdIn.readLine();
-                System.out.println("ora finish: ");
-                String oraFinish = stdIn.readLine();
-                commandString += nume + "|" + cnp + "|" + data + "|" + locatie + "|" + tipTratament + "|" + oraStart + "|" + oraFinish;
-            } else if (Objects.equals(command, "plata")) {
-                System.out.println("data: ");
-                String data = stdIn.readLine();
-                System.out.println("cnp: ");
-                String cnp = stdIn.readLine();
-                System.out.println("suma: ");
-                String suma = stdIn.readLine();
-                commandString += data + "|" + cnp + "|" + suma;
-            }
+            String nume = "a";
+            String cnp = "b";
+            String data = "c";
+
+            int locatie = rand.nextInt(5);
+            locatie++;
+
+            int tipTratament = rand.nextInt(5);
+            tipTratament++;
+
+            int oraStart = rand.nextInt(8);
+            oraStart += 10;
+            int oraFinish = oraStart + rand.nextInt(8) + 1;
+            if (oraFinish > 18) oraFinish = 18;
+
+            commandString = "programare|";
+            commandString += nume + "|" + cnp + "|" + data + "|" + locatie + "|" + tipTratament + "|" + oraStart + "|" + oraFinish;
 
             try (
                     Socket socket = new Socket(hostName, portNumber);
@@ -65,6 +52,39 @@ public class Client {
                 result = in.readLine();
             }
             System.out.println(result);
+
+            data = "c";
+            cnp = "b";
+            int programare = Integer.parseInt(result);
+
+            commandString = "plata|";
+            commandString += data + "|" + cnp + "|" + programare;
+
+            try (
+                    Socket socket = new Socket(hostName, portNumber);
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(socket.getInputStream()));
+            ) {
+                out.println(commandString);
+                result = in.readLine();
+            }
+            System.out.println(result);
+
+            if (rand.nextDouble() > 0.5) {
+                commandString = "anulare|";
+
+                try (
+                        Socket socket = new Socket(hostName, portNumber);
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                        BufferedReader in = new BufferedReader(
+                                new InputStreamReader(socket.getInputStream()));
+                ) {
+                    out.println(commandString);
+                    result = in.readLine();
+                }
+                System.out.println(result);
+            }
         }
     }
 }
